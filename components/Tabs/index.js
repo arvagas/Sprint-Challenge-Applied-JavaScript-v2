b2cb20c824fc1fba5e93abdf31ad342ff19c3b2d
Arvin Agas <arvagas@gmail.com>
2019-07-12 15:42:31 -0400

    added ability to deselect tab

--- components/Tabs/index.js
+++ components/Tabs/index.js
@@ -43,14 +43,20 @@ function selectTab(clickedTab) {
     const tabs = document.querySelectorAll('.tab')
     const cards = document.querySelectorAll('.card')
 
-    tabs.forEach(tab => tab.classList.remove('active-tab'))
+    if (clickedTab.classList.contains('active-tab')) {
+        clickedTab.classList.remove('active-tab')
 
-    cards.forEach(card => card.style.display='none')
+        cards.forEach(card => card.style.display='block')
+    } else {
+        tabs.forEach(tab => tab.classList.remove('active-tab'))
 
-    clickedTab.classList.add('active-tab')
-
-    cards.forEach(card => {
-        if (card.dataset.tab === clickedTab.dataset.tab) card.style.display = 'block'
-        // node.js will not have any cards because the article and topic api list them differently (topic[node.js] & article[node])
-    })
+        cards.forEach(card => card.style.display='none')
+    
+        clickedTab.classList.add('active-tab')
+    
+        cards.forEach(card => {
+            if (card.dataset.tab === clickedTab.dataset.tab) card.style.display = 'block'
+            // node.js will not have any cards because the article and topic api list them differently (topic[node.js] & article[node])
+        })
+    }
 }
