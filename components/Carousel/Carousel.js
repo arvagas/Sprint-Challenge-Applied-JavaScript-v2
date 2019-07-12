732f0503a72432dd95651eefae339bac7cf3b03e
Arvin Agas <arvagas@gmail.com>
2019-07-12 13:06:30 -0400

    carousel functional

--- components/Carousel/Carousel.js
+++ components/Carousel/Carousel.js
@@ -39,12 +39,50 @@ function createCarousel() {
 
   // Set attributes
   leftBtn.textContent = '<'
+  imgOne.src = './assets/carousel/mountains.jpeg'
+  imgTwo.src = './assets/carousel/computer.jpeg'
+  imgThree.src = './assets/carousel/trees.jpeg'
+  imgFour.src = './assets/carousel/turntable.jpeg'
   rightBtn.textContent = '>'
 
   // Set classes
   carousel.classList.add('carousel')
   leftBtn.classList.add('left-button')
+  imgOne.classList.add('img-reveal')
   rightBtn.classList.add('right-button')
 
+  // Button events
+  rightBtn.addEventListener('click', () => {
+    if (imgOne.classList.contains('img-reveal')) {
+      imgOne.classList.toggle('img-reveal')
+      imgTwo.classList.toggle('img-reveal')
+    } else if (imgTwo.classList.contains('img-reveal')) {
+      imgTwo.classList.toggle('img-reveal')
+      imgThree.classList.toggle('img-reveal')
+    } else if (imgThree.classList.contains('img-reveal')) {
+      imgThree.classList.toggle('img-reveal')
+      imgFour.classList.toggle('img-reveal')
+    } else if (imgFour.classList.contains('img-reveal')) {
+      imgFour.classList.toggle('img-reveal')
+      imgOne.classList.toggle('img-reveal')
+    }
+  })
+
+  leftBtn.addEventListener('click', () => {
+    if (imgOne.classList.contains('img-reveal')) {
+      imgOne.classList.toggle('img-reveal')
+      imgFour.classList.toggle('img-reveal')
+    } else if (imgFour.classList.contains('img-reveal')) {
+      imgFour.classList.toggle('img-reveal')
+      imgThree.classList.toggle('img-reveal')
+    } else if (imgThree.classList.contains('img-reveal')) {
+      imgThree.classList.toggle('img-reveal')
+      imgTwo.classList.toggle('img-reveal')
+    } else if (imgTwo.classList.contains('img-reveal')) {
+      imgTwo.classList.toggle('img-reveal')
+      imgOne.classList.toggle('img-reveal')
+    }
+  })
+  
   return carousel
 }
